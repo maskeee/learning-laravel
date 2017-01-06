eb8bb9ed356ce182d5cd894c34ed3bff9180b281
Jake <231005467@qq.com>
2017-01-06 03:49:56 +0000

    All User view & Data seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,10 +12,13 @@
 */
 
 $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
-    ];
+    $date_time = $faker->date . ' ' . $faker->time;
+       return [
+           'name' => $faker->name,
+           'email' => $faker->safeEmail,
+           'password' => str_random(10),
+           'remember_token' => str_random(10),
+           'created_at' => $date_time,
+           'updated_at' => $date_time,
+       ];
 });
